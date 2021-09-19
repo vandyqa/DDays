bbb4685bc5da3fc5ada4fc08a07e35b13c2bc1a3
vandyqa <78463661+vandyqa@users.noreply.github.com>
2021-09-19 17:28:23 -0500

    Update UtilityBase.java

--- src/main/java/resources/UtilityBase.java
+++ src/main/java/resources/UtilityBase.java
@@ -18,7 +18,6 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
-
 public class UtilityBase {
 	
 	public WebDriver driver;
@@ -27,52 +26,51 @@ public class UtilityBase {
 	public WebDriver initializeDriver() throws IOException
 	{
 		
-		prop = new Properties();
-		FileInputStream fis=new FileInputStream("C:\\Users\\mkalra\\eclipse-workspace\\DollarDaysMaven\\src\\main\\java\\resources\\data.properties");
-		prop.load(fis);
-		String browserName=prop.getProperty("browser");
-		System.out.println(browserName);
+	prop = new Properties();
+	FileInputStream fis=new FileInputStream("C:\\Users\\mkalra\\eclipse-workspace\\DollarDaysMaven\\src\\main\\java\\resources\\data.properties");
+	prop.load(fis);
+	String browserName=prop.getProperty("browser");
+	System.out.println(browserName);
 		
 	if (browserName.equals("chrome"))
 		
-		{	
+	{	
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mkalra\\Desktop\\Vandana-Java\\Silenium Drivers\\chromedriver\\chromedriver_win32\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\mkalra\\Desktop\\Vandana-Java\\Silenium Drivers\\chromedriver\\chromedriver_win32\\chromedriver.exe");
 		// TODO Auto-generated method stub
-		driver=new ChromeDriver();
+	driver=new ChromeDriver();
 			
-		}
+	}
 
 		
-		else if(browserName.equals("firefox"))
-		{
+	else if(browserName.equals("firefox"))
+	{
 			
 			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\mkalra\\Desktop\\Vandana-Java\\Silenium Drivers\\chromedriver\\chromedriver_win32\\chromedriver.exe");
 			//firefox code
 			
-		}
+	}
 		
-		else if (browserName.equals("IE"))
+	else if (browserName.equals("IE"))
 			
-		{
+	{
 			
 			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\mkalra\\Desktop\\Vandana-Java\\Silenium Drivers\\chromedriver\\chromedriver_win32\\chromedriver.exe");
 			//IE code
-		}
-	
+	}
 	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	return driver;
-}
+        }
 	
 	public String getScreenShotPath(String testCaseName, WebDriver driver) throws IOException
 	{
 	//Screenshot 
-		TakesScreenshot ts = (TakesScreenshot)  driver;
+	TakesScreenshot ts = (TakesScreenshot)  driver;
 	File source =   ts.getScreenshotAs(OutputType.FILE);
 	String destinationFile = System.getProperty("user.dir") +"\\reports\\"+testCaseName+".png";
-       FileUtils.copyFile(source,  new File(destinationFile));
-       return destinationFile;
-}
+        FileUtils.copyFile(source,  new File(destinationFile));
+        return destinationFile;
+        }
 
 	//private TakesScreenshot driver() {
 		// TODO Auto-generated method stub
